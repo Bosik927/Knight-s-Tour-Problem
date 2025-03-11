fbbb01dbd1cf2ada37b03a49550bed7b20d40ff3
Bosik927 <bosik927@gmail.com>
2025-03-11 21:49:06 +0100

    Brute force all calculations

--- src/main/kotlin/com/example/demo/test/KnightAllPositionsChecker.kt
+++ src/main/kotlin/com/example/demo/test/KnightAllPositionsChecker.kt
@@ -5,40 +5,44 @@ fun main(args: Array<String>) {
 }
 
 class KnightAllPositionsChecker(
-    private val width: Int = 8,
-    private val height: Int = 8
+    private val width: Int = 8, private val height: Int = 8
 ) {
 
 
-    fun checkAllPositions(): Boolean {
+    fun checkAllPositions() {
         if (width < 3 || height < 3) {
             println("Board is too small")
-            return false;
+            return
         }
         val calculator = KnightPositionsCalculator(width, height)
 
+        val startTime = System.currentTimeMillis()
         for (x in 0 until width) {
             for (y in 0 until height) {
                 println("Checking for starting position: $x, $y")
                 val visitedBoard = Array(width) { IntArray(height) }
                 visitedBoard[x][y] = 1
-                deepCalculation(visitedBoard, calculator, x, y)
+                deepCalculation(visitedBoard, calculator, x, y, 1)
             }
         }
+        val endTime = System.currentTimeMillis()
+        println("Time taken: ${endTime - startTime} ms")
 
-        return false;
+        return
     }
 
 
-    fun deepCalculation(visitedBoard: Array<IntArray>, calculator: KnightPositionsCalculator, x: Int, y: Int) {
+    fun deepCalculation(
+        visitedBoard: Array<IntArray>, calculator: KnightPositionsCalculator, x: Int, y: Int, count: Int
+    ) {
         if (visitedBoard.all { it.all { it > 0 } }) {
             println("All positions visited")
             println(visitedBoard)
             return
         }
 
-        val possiblePositions = calculator.calculate(x, y)
-
+        val possiblePositions =
+            calculator.calculate(x + 1, y + 1).filter { (nx, ny) -> visitedBoard[nx - 1][ny - 1] == 0 }
         if (possiblePositions.isEmpty()) {
             println("No more possible moves")
             return
@@ -47,119 +51,12 @@ class KnightAllPositionsChecker(
         possiblePositions.forEach {
             //Jump
             val copy = visitedBoard.copyOf()
-            copy[it.first][it.second] = 1
+            val americanX = it.first - 1
+            val americanY = it.second - 1
+            copy[americanX][americanY] = count
             deepCalculation(
-                copy,
-                calculator,
-                it.first,
-                it.second
+                copy, calculator, americanX, americanY, count + 1
             )
         }
     }
 }
-
-
-fun calculatePossibleKnightPositions(
-    visitedBoard: Array<BooleanArray>,
-    x: Int,
-    y: Int,
-    width: Int,
-    height: Int
-): List<Pair<Int, Int>> {
-    val positions = mutableListOf<Pair<Int, Int>>()
-    calcPosition1(visitedBoard, x, y, width, height)?.let { positions.add(it) }
-    calcPosition2(visitedBoard, x, y, width, height)?.let { positions.add(it) }
-    calcPosition3(visitedBoard, x, y, width, height)?.let { positions.add(it) }
-    calcPosition4(visitedBoard, x, y, width, height)?.let { positions.add(it) }
-    calcPosition5(visitedBoard, x, y, width, height)?.let { positions.add(it) }
-    calcPosition6(visitedBoard, x, y, width, height)?.let { positions.add(it) }
-    calcPosition7(visitedBoard, x, y, width, height)?.let { positions.add(it) }
-    calcPosition8(visitedBoard, x, y, width, height)?.let { positions.add(it) }
-    return positions;
-}
-
-fun calcPosition1(visitedBoard: Array<BooleanArray>, x: Int, y: Int, width: Int, height: Int): Pair<Int, Int>? {
-    val position1X = x - 2
-    val position2Y = y - 1;
-
-    if (isPossibleSquare(visitedBoard, position1X, position2Y, width, height)) {
-        return Pair(position1X, position2Y)
-    }
-    return null
-}
-
-fun calcPosition2(visitedBoard: Array<BooleanArray>, x: Int, y: Int, width: Int, height: Int): Pair<Int, Int>? {
-    val position1X = x - 1
-    val position2Y = y - 2;
-
-    if (isPossibleSquare(visitedBoard, position1X, position2Y, width, height)) {
-        return Pair(position1X, position2Y)
-    }
-    return null
-}
-
-fun calcPosition3(visitedBoard: Array<BooleanArray>, x: Int, y: Int, width: Int, height: Int): Pair<Int, Int>? {
-    val position1X = x - 2
-    val position2Y = y + 1
-    if (isPossibleSquare(visitedBoard, position1X, position2Y, width, height)) {
-        return Pair(position1X, position2Y)
-    }
-    return null
-}
-
-fun calcPosition4(visitedBoard: Array<BooleanArray>, x: Int, y: Int, width: Int, height: Int): Pair<Int, Int>? {
-    val position1X = x - 1
-    val position2Y = y + 2
-    if (isPossibleSquare(visitedBoard, position1X, position2Y, width, height)) {
-        return Pair(position1X, position2Y)
-    }
-    return null
-}
-
-fun calcPosition5(visitedBoard: Array<BooleanArray>, x: Int, y: Int, width: Int, height: Int): Pair<Int, Int>? {
-    val position1X = x + 1
-    val position2Y = y + 2
-    if (isPossibleSquare(visitedBoard, position1X, position2Y, width, height)) {
-        return Pair(position1X, position2Y)
-    }
-    return null
-}
-
-fun calcPosition6(visitedBoard: Array<BooleanArray>, x: Int, y: Int, width: Int, height: Int): Pair<Int, Int>? {
-    val position1X = x + 2
-    val position2Y = y + 1
-    if (isPossibleSquare(visitedBoard, position1X, position2Y, width, height)) {
-        return Pair(position1X, position2Y)
-    }
-    return null
-}
-
-fun calcPosition7(visitedBoard: Array<BooleanArray>, x: Int, y: Int, width: Int, height: Int): Pair<Int, Int>? {
-    val position1X = x + 2
-    val position2Y = y - 1
-    if (isPossibleSquare(visitedBoard, position1X, position2Y, width, height)) {
-        return Pair(position1X, position2Y)
-    }
-    return null
-}
-
-fun calcPosition8(visitedBoard: Array<BooleanArray>, x: Int, y: Int, width: Int, height: Int): Pair<Int, Int>? {
-    val position1X = x + 1
-    val position2Y = y - 2
-    if (isPossibleSquare(visitedBoard, position1X, position2Y, width, height)) {
-        return Pair(position1X, position2Y)
-    }
-    return null
-}
-
-private fun isPossibleSquare(
-    visitedBoard: Array<BooleanArray>,
-    position1X: Int,
-    position2Y: Int,
-    width: Int,
-    height: Int
-): Boolean {
-    return position1X > 0 && position2Y > 0 && position1X < width && position2Y < height
-            && !visitedBoard[position1X][position2Y]
-}
-
